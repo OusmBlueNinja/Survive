99b721e08c3cb01ef803ec9ac6962d10942aac4b
Gigabite Studio's LLC <89956790+OusmBlueNinja@users.noreply.github.com>
2022-10-15 00:13:52 -0500

    made graphics library (fully custom)

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include <stdlib.h>
 
+#include <conio.h>
+
 // my custom standard file that contains simple functions like delay
 #include "stnd.h"
 //file handeler
@@ -16,59 +18,74 @@ int oldPlayerPos;
 int player = 5;
 
 int main(int argc, char **argv) {
-	int Frame[100] = {
-1,1,1,1,1,1,1,1,1,1,
-1,0,0,0,0,0,0,0,0,1,
-1,0,0,0,0,0,0,0,0,1,
-1,0,0,0,0,0,0,0,0,1,
-1,0,0,0,0,0,0,0,0,1,
-1,0,0,0,0,0,0,0,0,1,
-1,0,0,0,0,0,0,0,0,1,
-1,0,0,0,0,0,0,0,0,1,
-1,0,0,0,0,0,0,0,0,1,
-1,1,1,1,1,1,1,1,1,1
+	int Frame[400] = {
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,10,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
   };
 	int i;
-	printf("Starting\n");
+	int option;
+	int Health = 5;
+	printf("click C top close\n");
 	delay(2);
 	
-		playerPos = 51;
+	playerPos = 55;
 
 	for (i = 0; i< 1000; i++) {
 	//set player pos on frame
+	printf("\n");
+	option = getch();
+	printf("%d", option);
+	if (option == 119){ 
+		playerPos = move(playerPos, Frame, player, 1);
+	} else if (option == 115){
+		playerPos = move(playerPos, Frame, player, 2);
+	} else if (option == 100){
+		playerPos = move(playerPos, Frame, player, 3);
+	} else if (option == 97){
+		playerPos = move(playerPos, Frame, player, 4);
+	} else if (option == 99) {
+		exit(0);
+
+	} else{
+		playerPos = playerPos;
+	}
+	
 	Frame[playerPos] = player;
 	//save the player pos so you can remove it
-	
+  if (playerPos != oldPlayerPos){
 	oldPlayerPos = playerPos;
-	
+	} else {
+	}
+
+	if ( enemyColide(playerPos, Frame, player) == 1 ) {
+		Health = Health - 1;
+	}
 	
 	//print the frame
 	printFrame(Frame);
+
+	printf("\nHealth: %d/5", Health);
 	//remove the player from frame
   
 	Frame[oldPlayerPos] = 0;
-	
-	// Check if the position that the player is being moves has something thire 
-	//if it does it doesent move the player
-	if (badPlayerpos(playerPos, Frame, player) == 0) {
-		playerPos = playerPos + 1;
-	} else if (badPlayerpos(playerPos, Frame, player) == 2){
-		// moves player to start of window
-		playerPos = 11;
-		
-	} else {
-		 playerPos = playerPos + 3;
 	}
-
-	
-
-	delay(0.5);
-	}
-	
-
-	
-
-	
-	
 	return 0;
 }
--- src/win.h
+++ src/win.h
@@ -7,8 +7,8 @@ void printFrame(int Frame[]) {
   int y = 0;
   int DEBUG = 0;
   system("cls");
-  for (i = 0; i< 100; i++) {
-    if (y == 10) {
+  for (i = 0; i< 400; i++) {
+    if (y == 20) {
       y = 0;
       printf("\n");
     }
@@ -31,6 +31,12 @@ void printFrame(int Frame[]) {
       printf("  0");
     } else if (Frame[i] == 8) {
       printf("  -");
+    } else if (Frame[i] == 9) {
+      printf("  |");
+    } else if (Frame[i] == 10) {
+      printf("(=)");
+    } else if (Frame[i] == 11) {
+      printf(")-(");
     }
   }
   
@@ -57,14 +63,52 @@ int badPlayerpos(int playerPos, int Frame[], int player) {
 
 }
 
-void move(int playerPos, int Frame[], int player, int direction) {
+int enemyColide(int playerPos, int Frame[], int player) {
+  if (Frame[playerPos] == 10) {
+    return 1;
+  } 
+}
+
+int move(int playerPos, int Frame[], int player, int direction) {
     if (direction == 1) { //up
+    int nextplayerpos = playerPos -20;
+    if (Frame[nextplayerpos] == 0) {
+      int newplayerpos = playerPos - 20;
+      return newplayerpos;
+    } else {
+      return playerPos;
+    } 
+    
 
-    } else if (direction == 2) {//down
+    } else if (direction == 2) { //up
+    int nextplayerpos = playerPos + 20;
+    if (Frame[nextplayerpos] == 0) {
+      int newplayerpos = playerPos + 20;
+      return newplayerpos;
 
-    } else if (direction == 3) {//left
+    } else {
+      return playerPos;
+    }
 
-    } else if (direction == 4) {//right
+    }else if (direction == 3) { //up
+    int nextplayerpos = playerPos + 1;
+    if (Frame[nextplayerpos] == 0) {
+      int newplayerpos = playerPos + 1;
+      return newplayerpos;
+
+    } else {
+      return playerPos;
+    }
+
+    }else if (direction == 4) { //up
+    int nextplayerpos = playerPos - 1;
+    if (Frame[nextplayerpos] == 0) {
+      int newplayerpos = playerPos - 1;
+      return newplayerpos;
+
+    } else {
+      return playerPos;
+    }
 
     }
     
